fd88a8aa4fe3098300534bee02c3248944630383
marlamoury <marlamoury@hotmail.com>
2025-03-23 21:54:07 -0300

    Adicionar arquivos de projeto.

---
+++ Consumer.Contact.Create.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Consumer.Contact.Create.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Consumer.Contact.Create.Worker/Program.cs
@@ -0,0 +1,34 @@
+using Consumer.Contact.Create.Worker;
+using Consumer.Create.Contact.Application.Services;
+using Consumer.Create.Contact.Infrastructure.Messaging;
+using Consumer.Create.Contact.Infrastructure.Persistence;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        // Carrega o arquivo appsettings.json
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        // Carrega as configurações do RabbitMQ do appsettings.json
+        var rabbitMqSettings = context.Configuration.GetSection("RabbitMQSettings").Get<RabbitMQSettings>();
+        services.AddSingleton(rabbitMqSettings);
+
+        // Registrando serviços e repositórios
+        services.AddScoped<IContatoRepository, ContatoRepository>();
+        services.AddScoped<IContatoService, ContatoService>();
+
+        // Registrando o RabbitMQConsumer como Singleton e o Worker como HostedService
+        services.AddSingleton<RabbitMQConsumer>();
+        services.AddHostedService<Worker>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+    })
+    .Build();
+
+await host.RunAsync();
+
